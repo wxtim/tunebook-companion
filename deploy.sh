75098c7ff9427e78fcac4bb280372a3efa5df2d6
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-03-13 18:25:55 +0000

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@ yarn run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAMEgit initgit add -A
+# echo 'www.example.com' > CNAME
+git init
+git add -A
 git commit -m 'deploy'
 git push -f git@github.com:wxtim/tunebook-companion.git master:gh-pages
 cd -
