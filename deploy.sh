3446763d64eeb1c1fdca3c65a4d415959badb2c2
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-03-13 18:17:36 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAMEgit initgit add -A
 git commit -m 'deploy'
-git push -f git@github.com:wxtim/tunebook-companion.git master:gh-pagescd -
+git push -f git@github.com:wxtim/tunebook-companion.git master:gh-pages
+cd -
