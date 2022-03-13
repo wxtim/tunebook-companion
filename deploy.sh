5594bc33540d592bdf47947926303a0851334ea4
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-03-13 18:13:26 +0000

    added some more files

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAMEgit initgit add -A
+git commit -m 'deploy'
+git push -f git@github.com:wxtim/tunebook-companion.git master:gh-pagescd -
