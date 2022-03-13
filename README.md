93e42f9dc41e6bb59c9e485f0df3aa5c275224e7
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-03-13 18:05:09 +0000

    so far

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# tunelookup-vue
+# Northumbrian Piping Tunebook Directory
+
+TODO - add link to github.io page here.
+
+An app designed to take a tune name and return
+location of tune in the specific editions of a
+tunebook owned by a given user.
+
+
+# Roadmap
+
+1. Build a proof of concept with simple test data.
+2. Deploy PoC on GH pages using GH actions
+   to build the project. - this should make the
+   deployment repeatable.
+3. Add backend data to produce a basic app.
+4. Add a means to save user preferences about
+   which tune books they have.
+5. Add an I'm feeling lucky.
+6. Add links to foltunefinder and/or the session
+   if tune exists but not in an edition you own.
+
+
+## Desired appearance:
+
+```
+Tune Name:
+
+Find this tune at:
+    - Location
+    - Location
+
+I have the following tunebooks
+Book 1  <dropdown selecting edition or None>
+Book 2  <dropdown selecting edition or None>
+Book 3  <dropdown selecting edition or None>
+Book 4  <dropdown selecting edition or None>
+Yellow Book  <dropdown selecting edition or None>
+Green Book  <dropdown selecting edition or None>
+Duet Book  <dropdown selecting edition or None>
+&c &c
+```
+
+# Developer information
 
 ## Project setup
 ```
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,45 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>NSP Books Lookup</h1>
+    <input v-model="message" placeholder="edit me">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(item) in tuneInfo" v-bind:key="item">
+        {{ item.tune }}, {{item.book}}, {{item.edition}}, {{item.page}}
+    </li>
     </ul>
   </div>
 </template>
 
 <script>
+import {bookData} from "./test.json";
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function () {
+    return {message: ''}
+  },
+  computed: {
+    tuneInfo: function(message) {
+
+      console.log(message)
+      console.log(bookData)
+
+      var tunes = [];
+      for (let book in bookData) {
+        for (let edition in bookData[book]) {
+          for (let tune in bookData[book][edition]) {
+            console.log(tune)
+            if (tune.toLowerCase().match(this.message.toLowerCase())) {
+              tunes.push({"tune":tune, "book":book, "edition": edition, "page":bookData[book][edition][tune]})
+            }
+            // eslint-disable-next-line
+            // debugger
+          }
+        }
+      }
+      console.log(tunes)
+      return tunes
+    }
   }
 }
 </script>
@@ -45,11 +50,11 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
+  /* list-style-type: none; */
   padding: 0;
 }
 li {
-  display: inline-block;
+  /* display: inline-block; */
   margin: 0 10px;
 }
 a {
