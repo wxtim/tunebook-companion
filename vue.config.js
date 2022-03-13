e8c4f5f83e89f752f37b205b46ea04af9089875e
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-03-13 18:55:17 +0000

    fpp

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production'
-  // ? '/tunebook-companion/'
-  // : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/tunebook-vue/'
+  : '/'
 })
