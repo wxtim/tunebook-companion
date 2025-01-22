09a43a7f8b3c41a7572365296df6842beed4103b
Tim Pillinger <tim.pillinger@metoffice.gov.uk>
2025-01-22 09:49:31 +0000

    added tunebooks

--- etc/json2csv.py
+++ etc/json2csv.py
@@ -1,20 +1,24 @@
+"""Convert this applications "database" (the json file) to a CSV
+
+to use
+
+python etc/json2csv.py
+"""
+
 import json
 from pathlib import Path
+
 
 PATH = Path(__file__).parent.parent / 'src/components/tunebooks.json'
 OUT_PATH = Path(__file__).parent / 'tunebooks.csv'
 
 
-def main():
-    data = json.loads(PATH.read_text())['bookData']
-    linerized_data = []
-    for book, editions in data.items():
-        for edition, tunes in editions.items():
-            for tune, page in tunes.items():
-                linerized_data.append([book, edition, tune, page])
+data = json.loads(PATH.read_text())['bookData']
+linerized_data = []
+for book, editions in data.items():
+    for edition, tunes in editions.items():
+        for tune, page in tunes.items():
+            linerized_data.append([book, edition, tune, page])
 
-    with open('../tunebooks.csv', 'w') as fh:
-        fh.write('\n'.join(['|'.join(i) for i in linerized_data]))
+OUT_PATH.write_text('\n'.join(['|'.join(i) for i in linerized_data]))
 
-if __name__ == '__main__':
-    main()
